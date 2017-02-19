1c2897f4b41e4cb8d9ccc24779af7016ce341247
EbonFog <bellazydog@gmail.com>
2017-02-19 17:36:02 +0700

    Add Remove & Checking Function

--- test.cpp
+++ test.cpp
@@ -1,41 +1,46 @@
-#include <iostream>
-#include <cstdlib>
 #include <pthread.h>
 #include <stdio.h>
-
-using namespace std;
+#include <conio.h>
 
 #define NUM_THREADS     5
 #define N_BUFFER        1000
 #define N_DATA          100000
 #define N_PRODUCER      20
-#defint N_CONSUMER      30
+#define N_CONSUMER      30
 
-void *add_item();
-void *remove_item();
-void *PrintHello(void *threadid);
+void *add_item(int buf[], int *tail);
+void *remove_item(int buf[], int *head);
+bool isEmpty(int head, int tail);
+bool isFull(int head, int tail);
 
 int main () {
-   pthread_t threads[NUM_THREADS];
-   int rc;
-   int i;
-
-   for( i=0; i < NUM_THREADS; i++ ){
-        printf("main() : creating thread, %d \n",i);
-        rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
-
-        if (rc){
-            printf("Error:unable to create thread, %d \n",rc);
-            exit(-1);
-        }
-   }
-
-   pthread_exit(NULL);
+    int buf[N_BUFFER] = {0};
+    int buf_head = 0;
+    int buf_tail = 0;
 }
 
-void *PrintHello(void *threadid) {
-   long tid;
-   tid = (long)threadid;
-   printf("Hello World! Thread %d' \n",tid);
-   pthread_exit(NULL);
+void *add_item(int buf[], int *tail){
+    buf[*tail] = 1;
+    *tail++;
+    if(*tail >= N_BUFFER)
+        *tail -= N_BUFFER;
+}
+
+void *remove_item(int buf[], int *head){
+    buf[*head] = 0;
+    *head++;
+    *head %= N_BUFFER;
+}
+
+bool isEmpty(int head, int tail){
+    if(*h == 0)
+        return true;
+    else
+        return false;
+}
+bool isFull(int head, int tail){
+    if(*t==1)
+        return true;
+    else
+        return false;
 }
