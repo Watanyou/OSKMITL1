b128ccd914ea996852328d6106214b4a9b33ad9c
EbonFog <bellazydog@gmail.com>
2017-02-23 23:20:58 +0700

    Try Improve #3

--- OSAssign1/main.cpp
+++ OSAssign1/main.cpp
@@ -13,12 +13,12 @@ int in_req=0;
 int out_req=0;
 int buf_req=0;
 
-void add_item(int buf[], int *tail_ptr);
-void remove_item(int buf[], int *head_ptr);
-bool isEmpty(int buf[], int head, int tail);
-bool isFull(int buf[], int head, int tail);
-void append(int id, int buf[],int *head_ptr,int *tail_ptr);
-void consume(int id, int buf[],int *head_ptr,int *tail_ptr);
+void add_item(bool buf[], int *tail_ptr);
+void remove_item(bool buf[], int *head_ptr);
+bool isEmpty(bool buf[], int head, int tail);
+bool isFull(bool buf[], int head, int tail);
+void append(int id, bool buf[],int *head_ptr,int *tail_ptr);
+void consume(int id, bool buf[],int *head_ptr,int *tail_ptr);
 bool isTimeout(double time);
 
 int main (int argc, char* argv[]) {
@@ -41,7 +41,7 @@ int main (int argc, char* argv[]) {
     cout<<"Requests "<<req_size<<endl<<endl;
 
     in_req = req_size;
-    int *buf = new int[buf_size];
+    bool *buf = new bool[buf_size];
     int buf_head = 0;
     int buf_tail = 0;
     thread producer[producer_size];
@@ -78,34 +78,28 @@ int main (int argc, char* argv[]) {
 
 }
 
-void add_item(int buf[], int *tail_ptr){
+void add_item(bool buf[], int *tail_ptr){
     buf[*tail_ptr] = 1;
     (*tail_ptr)++;
     if(*tail_ptr >= buf_size)
         *tail_ptr -= buf_size;
 }
 
-void remove_item(int buf[], int *head_ptr){
+void remove_item(bool buf[], int *head_ptr){
     buf[*head_ptr] = 0;
     (*head_ptr)++;
     if(*head_ptr >= buf_size)
         *head_ptr -= buf_size;
 }
 
-bool isEmpty(int buf[], int head, int tail){
-    if(buf[head] == 0)
-        return true;
-    else
-        return false;
+bool isEmpty(bool buf[], int head, int tail){
+    return !buf[head];
 }
-bool isFull(int buf[], int head, int tail){
-    if(buf[tail]==1)
-        return true;
-    else
-        return false;
+bool isFull(bool buf[], int head, int tail){
+    return buf[tail];
 }
 
-void append(int id, int buf[],int *head_ptr,int *tail_ptr){
+void append(int id, bool buf[],int *head_ptr,int *tail_ptr){
     double timer=0;
     while(1){
         m.lock();
@@ -133,7 +127,7 @@ void append(int id, int buf[],int *head_ptr,int *tail_ptr){
         m.unlock();
     }
 }
-void consume(int id, int buf[],int *head_ptr,int *tail_ptr){
+void consume(int id, bool buf[],int *head_ptr,int *tail_ptr){
     while(1){
         m.lock();
         if(in_req > 0 || !isEmpty(buf,*head_ptr, *tail_ptr)){
