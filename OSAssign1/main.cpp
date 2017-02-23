2b9cae1f4e542567243b9e85f2096e5785dfaa09
EbonFog <bellazydog@gmail.com>
2017-02-23 21:08:24 +0700

    Try Improve #2 -Fuck อิอิ

--- OSAssign1/main.cpp
+++ OSAssign1/main.cpp
@@ -13,12 +13,12 @@ int in_req=0;
 int out_req=0;
 int buf_req=0;
 
-void add_item(int buf[], int *tail_ptr);
-void remove_item(int buf[], int *head_ptr);
-bool isEmpty(int buf[], int head, int tail);
-bool isFull(int buf[], int head, int tail);
-void append(int id, int buf[],int *head_ptr,int *tail_ptr);
-void consume(int id, int buf[],int *head_ptr,int *tail_ptr);
+void add_item(bool buf[], int *tail_ptr);
+void remove_item(bool buf[], int *head_ptr);
+bool isEmpty(bool buf[], int head, int tail);
+bool isFull(bool buf[], int head, int tail);
+void append(bool buf[],int *head_ptr,int *tail_ptr);
+void consume(bool buf[],int *head_ptr,int *tail_ptr);
 bool isTimeout(double time);
 
 int main (int argc, char* argv[]) {
@@ -29,22 +29,22 @@ int main (int argc, char* argv[]) {
 
     buf_size = atoi(argv[3]);
     in_req = atoi(argv[4]);
-    int *buf = new int[buf_size];
+    bool *buf = new bool[buf_size];
     int buf_head = 0;
     int buf_tail = 0;
     thread producer[atoi(argv[1])];
     thread consumer[atoi(argv[2])];
 
     for(int i = 0; i< buf_size; i++)
-        buf[i] = 0;
+        buf[i] = false;
 
     auto startTime = std::chrono::high_resolution_clock::now();
 
     for(int i= 0;i<atoi(argv[1]);i++){
-        producer[i] = thread(append, i, buf, &buf_head, &buf_tail);
+        producer[i] = thread(append, buf, &buf_head, &buf_tail);
     }
     for(int i= 0;i<atoi(argv[2]);i++){
-        consumer[i] = thread(consume, i, buf, &buf_head, &buf_tail);
+        consumer[i] = thread(consume, buf, &buf_head, &buf_tail);
     }
     for(int i=0 ;i<atoi(argv[1]);i++){
         producer[i].join();
@@ -66,34 +66,28 @@ int main (int argc, char* argv[]) {
 
 }
 
-void add_item(int buf[], int *tail_ptr){
-    buf[*tail_ptr] = 1;
+void add_item(bool buf[], int *tail_ptr){
+    buf[*tail_ptr] = true;
     (*tail_ptr)++;
     if(*tail_ptr >= buf_size)
         *tail_ptr -= buf_size;
 }
 
-void remove_item(int buf[], int *head_ptr){
-    buf[*head_ptr] = 0;
+void remove_item(bool buf[], int *head_ptr){
+    buf[*head_ptr] = false;
     (*head_ptr)++;
     if(*head_ptr >= buf_size)
         *head_ptr -= buf_size;
 }
 
-bool isEmpty(int buf[], int head, int tail){
-    if(buf[head] == 0)
-        return true;
-    else
-        return false;
+bool isEmpty(bool buf[], int head, int tail){
+    return !buf[head];
 }
-bool isFull(int buf[], int head, int tail){
-    if(buf[tail]==1)
-        return true;
-    else
-        return false;
+bool isFull(bool buf[], int head, int tail){
+    return buf[tail];
 }
 
-void append(int id, int buf[],int *head_ptr,int *tail_ptr){
+void append(bool buf[],int *head_ptr,int *tail_ptr){
     double timer=0;
     while(1){
         m.lock();
@@ -121,7 +115,7 @@ void append(int id, int buf[],int *head_ptr,int *tail_ptr){
         m.unlock();
     }
 }
-void consume(int id, int buf[],int *head_ptr,int *tail_ptr){
+void consume(bool buf[],int *head_ptr,int *tail_ptr){
     while(1){
         m.lock();
         if(in_req > 0 || !isEmpty(buf,*head_ptr, *tail_ptr)){
